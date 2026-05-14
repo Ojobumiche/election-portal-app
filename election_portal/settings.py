4906bfc3674939e3ec88ea1db88eedd2a33874d1
Ojobumiche <94999005+Ojobumiche@users.noreply.github.com>
2026-05-14 19:31:32 +0100

    database config settings updated

--- election_portal/settings.py
+++ election_portal/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'election_portal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'bincom_test'),
-        'USER': os.getenv('DB_USER', 'root'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'chenemi'),
-        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
